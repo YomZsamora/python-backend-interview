4f4bf6c330ece2a2823b9e03e5ba3379a19e0536
yomZsamora <samaurah@gmail.com>
2022-10-31 11:42:45 +0300

    Made the Trips app modifiable in the admin.

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Trip, Driver, Vehicle, Customer
+
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(Driver)
+admin.site.register(Customer)
+admin.site.register(Vehicle)
